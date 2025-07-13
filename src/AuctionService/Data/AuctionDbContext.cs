53e7817995c45d567386f4163c3a2a66277aea42
mohammed.Zr <mohamed.zrirake@gmail.com>
2025-07-13 18:43:06 +0100

    Initial_Commit_First_Time_Working_Whit_Microservices

---
+++ src/AuctionService/Data/AuctionDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.data
+{
+    public class AuctionDbContext:DbContext
+    {
+        public AuctionDbContext(DbContextOptions<AuctionDbContext> options) : base(options)
+        {
+        }
+        
+        public DbSet<Entities.Auction> Auctions { get; set; }
+     
+
+    }
+}
